32acf93d8526cf2f03f7ea2752a23b41c96c74cf
shiv loka <shiv.loka@appdynamics.com>
2016-09-20 16:42:57 -0700

    Made changes to Maps Activity - Added Async Tasks for location updates/UI updates and for reverse Geocoding

--- app/src/main/java/org/gowind/MapsActivity.java
+++ app/src/main/java/org/gowind/MapsActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -11,9 +12,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -27,16 +26,15 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import org.gowind.api.LocationAddress;
 import org.gowind.util.PermissionUtils;
 
 import java.io.IOException;
@@ -44,6 +42,7 @@ import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -53,18 +52,20 @@ import io.fabric.sdk.android.services.concurrency.AsyncTask;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener,
         GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks,
-        LocationListener, AdapterView.OnItemClickListener, AdapterView.OnItemSelectedListener {
+        LocationListener {
 
     //TODO: Use butterknife
 
 
     private static final String LOGTAG = "Gowind-MapsActivity";
+    private static final int NUMBER_OF_ADDRESSES = 3;
+    private static final int MINIMUM_DROPDOWN_THRESHOLD = 3;
     private final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
     GoogleMap mMap;
     @BindView(R.id.requestButton) Button requestButton;
     @BindView(R.id.originAutoComplete) AutoCompleteTextView originAutoComplete;
-    //AutoCompleteTextView originAutoComplete;
+    @BindView(R.id.destinationAutoComplete) AutoCompleteTextView destinationAutoComplete;
     @BindView(R.id.paymentsButton) ImageButton paymentsButton;
     @BindView(R.id.profileButton) ImageButton profileButton;
     @BindView(R.id.settingsButton) ImageButton settingsButton;
@@ -77,8 +78,27 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private Marker mMarker;
     private LinearLayout mapsLayout;
     private boolean mPermissionDenied = false;
-    private List<String> stringAddressList = new ArrayList<>();
 
+    private LatLng mCurrentLatLng;
+
+    public LatLng getmCurrentLatLng() {
+        return mCurrentLatLng;
+    }
+
+    public void setmCurrentLatLng(LatLng mCurrentLatLng) {
+        this.mCurrentLatLng = mCurrentLatLng;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,8 +106,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_maps);
         ButterKnife.bind(this);
-
-    //    originAutoComplete = (AutoCompleteTextView) findViewById(R.id.originAutoComplete);
 
         // Create an instance of GoogleAPIClient.
         if (mGoogleApiClient == null) {
@@ -98,18 +116,16 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     .build();
         }
 
-
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        setupBottomPanelButtonListeners();
+        setUpViewListeners();
     }
 
-    private void setupBottomPanelButtonListeners() {
+    private void setUpViewListeners() {
 
-        originAutoComplete.setOnItemSelectedListener(this);
         requestButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -140,35 +156,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 //TODO : Create settings activity and pass intent
             }
         });
-    }
-
-    //TODO: Offload locationServices to a different Thread / asyncTask
-    //TODO: Reduce weight on the main thread.
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.updateProfile:
-                // User chose the "Favorite" action, mark the current item
-                // as a favorite...
-                Log.i(LOGTAG, "User Menu selected");
-                Intent userProfileIntent = new Intent(MapsActivity.this, UserProfileActivity.class);
-                startActivity(userProfileIntent);
-                return true;
-
-            case R.id.addPayment:
-
-                Log.i(LOGTAG, "Add payment selected");
-                Intent userPaymentIntent = new Intent(MapsActivity.this, PaymentActivity.class);
-                startActivity(userPaymentIntent);
-                return true;
-
-            default:
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                return super.onOptionsItemSelected(item);
-
-        }
     }
 
     @Override
@@ -218,7 +205,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public boolean onMyLocationButtonClick() {
-        enableMyLocation();
         return false;
     }
 
@@ -232,6 +218,28 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .setFastestInterval(5000);
         mRequestingLocationUpdates = true;
         enableMyLocation();
+    }
+
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+            //to get current location
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+        }
+        Location myCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (myCurrentLocation != null) {
+            new UpdateMarkerTask().execute(myCurrentLocation);
+        }
+        //to receive location updates upon movement.
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
     }
 
     @Override
@@ -265,9 +273,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onLocationChanged(Location location) {
         mCurrentLocation = location;
-        String hello = "I am in Location Changed";
-        Log.i(LOGTAG, hello);
-        new UpdateUILocationTask().execute(mCurrentLocation);
+        new UpdateMarkerTask().execute(mCurrentLocation);
+        Log.i(LOGTAG, getmCurrentLatLng().latitude + " : " + getmCurrentLatLng().longitude);
+        new UpdateTextViewAdapterTask().execute(getmCurrentLatLng());
     }
 
     private void stopLocationUpdates() {
@@ -275,103 +283,88 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 mGoogleApiClient, this);
     }
 
-
     /**
-     * Enables the My Location layer if the fine location permission has been granted.
+     * This class has the following functions:
+     * 1. upon change in location, the onLocationChanged method passes the location to this class
+     * 2. this class takes the location parameter and passes the LatLng to onPostExecute method
+     * 3. The onPostExecute() updates the view (marker and map) on the UI thread with the latlng.
      */
-    private void enableMyLocation() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            // Permission to access the location is missing.
-            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
-                    Manifest.permission.ACCESS_FINE_LOCATION, true);
-        } else if (mMap != null) {
-            // Access to the location has been granted to the app.
-            mMap.setMyLocationEnabled(true);
-        }
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-    }
 
-    private class UpdateUILocationTask extends AsyncTask<Location, Integer, LatLng> {
+    private class UpdateMarkerTask extends AsyncTask<Location, Integer, LatLng> {
+        @Override
         protected LatLng doInBackground(Location... locations) {
-            Log.i(LOGTAG, "Inside Async task updating location address");
-            List<Address> addressList;
+            Thread.currentThread().setName("LocationUpdater");
             LatLng latLng = null;
             Location userLocation = locations[0];
             if (userLocation != null) {
                 String lastLocationUpdateTime = DateFormat.getTimeInstance().format(new Date());
-
-                Log.i(LOGTAG, userLocation.getLatitude() + " :: " +
-                        userLocation.getLongitude() + " : update time: " +lastLocationUpdateTime);
+                Log.i(LOGTAG, userLocation.getLatitude() + " : " +
+                        userLocation.getLongitude() + " : update time: " + lastLocationUpdateTime);
                 latLng = new LatLng(userLocation.getLatitude(), userLocation.getLongitude());
-
-                try {
-                    LocationAddress locAddress = new LocationAddress();
-                    addressList = locAddress.getAddressFromLocation(MapsActivity.this, userLocation);
-
-                    if (addressList != null && addressList.size() > 0) {
-                        for (Address address : addressList) {
-                            Log.i(LOGTAG, ":::" + address.getCountryCode() + address.getFeatureName());
-                            stringAddressList.add(getAddressAsString(address));
-                        }
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                if (mMarker != null) {
-                                    mMarker.remove();
-                                }
-                                ArrayAdapter<String> autoCompleteTextViewAdapter =
-                                        new ArrayAdapter<>(MapsActivity.this, android.R.layout.simple_dropdown_item_1line, stringAddressList);
-                                autoCompleteTextViewAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                                originAutoComplete.setAdapter(autoCompleteTextViewAdapter);
-                            }
-                        });
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
             }
             return latLng;
-        }
-
-        /**
-         * Helper method to convert a given Address Object to String -
-         * returns Location, city, state, feature name, country code, postal code.
-         * @param address
-         * @return
-         */
-        private String getAddressAsString(Address address) {
-            StringBuffer sb = new StringBuffer();
-            if (address.getFeatureName().isEmpty()) {
-                sb.append(address.getAddressLine(0)).append(" ")
-                        .append(address.getAdminArea()).append(" ")
-                        .append(address.getCountryCode());
-            }
-            sb.append(address.getFeatureName()).append(" ")
-                    .append(address.getAddressLine(0)).append(" ")
-                    .append(address.getLocality()).append(" ")
-                    .append(address.getAdminArea()).append(" ")
-                    .append(address.getCountryCode()).append(" ");
-
-            return sb.toString();
         }
 
         @Override
         protected void onPostExecute(LatLng latLng) {
             final LatLng latLong = latLng;
-           if (latLng != null) {
-               runOnUiThread(new Runnable() {
-                   @Override
-                   public void run() {
-                       MarkerOptions markerOptions = new MarkerOptions()
-                               .position(latLong)
-                               .title("Current Position")
-                               .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
-                       mMarker = mMap.addMarker(markerOptions);
-                       mMap.moveCamera(CameraUpdateFactory.newLatLng(latLong));
-                   }
-               });
-           }
+
+            setmCurrentLatLng(latLong);
+            if (mMarker != null) {
+                mMarker.remove();
+            }
+            MarkerOptions markerOptions = new MarkerOptions()
+                    .position(latLong)
+                    .title("Current Position");
+            mMarker = mMap.addMarker(markerOptions);
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLong));
+            CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
+            mMap.animateCamera(zoom);
+         }
+    }
+
+    private class UpdateTextViewAdapterTask extends AsyncTask<LatLng, Void, List<String>> {
+        @Override
+        protected List<String> doInBackground(LatLng... latLngs) {
+            Thread.currentThread().setName("TextViewAdapterUpdater");
+
+            LatLng latLng = latLngs[0];
+            List<Address> addresses = new ArrayList<>();
+            String errorMessage;
+            List<String> stringAddresses = new ArrayList<>();
+            if (latLng != null) {
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                try {
+                    addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, NUMBER_OF_ADDRESSES);
+                } catch (IOException ioException) {
+                    errorMessage = "Service Not Available";
+                    Log.e(LOGTAG, errorMessage, ioException);
+                } catch (IllegalArgumentException illegalArgumentException) {
+                    errorMessage = "Invalid Latitude or Longitude Used";
+                    Log.e(LOGTAG, errorMessage + ". " +
+                            "Latitude = " + latLng.latitude + ", Longitude = " +
+                            latLng.longitude, illegalArgumentException);
+                }
+            }
+            if (addresses != null && addresses.size() > 0) {
+                for (Address address : addresses) {
+                    stringAddresses.add(address.getAddressLine(0));
+                }
+            }
+            return stringAddresses;
+        }
+
+        @Override
+        protected void onPostExecute(List<String> stringAddresses) {
+
+            if (stringAddresses.size() > 0) {
+                ArrayAdapter<String> autoCompleteTextViewAdapter = new ArrayAdapter<>(MapsActivity.this, android.R.layout.simple_dropdown_item_1line, stringAddresses);
+                autoCompleteTextViewAdapter.setNotifyOnChange(true);
+                autoCompleteTextViewAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                originAutoComplete.setAdapter(autoCompleteTextViewAdapter);
+                originAutoComplete.setThreshold(MINIMUM_DROPDOWN_THRESHOLD);
+                autoCompleteTextViewAdapter.notifyDataSetChanged();
+            }
         }
     }
 
@@ -394,19 +387,5 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .newInstance(true).show(getSupportFragmentManager(), "dialog");
     }
     /** END MAP/LOCATION RELATED CODE **/
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-    }
 
 }
